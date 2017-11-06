1a66f25297dc8f5711e5c6bdfea4ab05370a4b67
unknown <jose.lagoa@ESTUDIO.LOCAL>
2017-11-06 19:59:37 +0100

    paso2 funcion sumaArray okey

---
+++ main.js
@@ -0,0 +1,44 @@
+"use strict"
+/**
+ * @fileOverview Este archivo contiene ejemplos de funciones con for
+ * @author Christian Vazquez
+ * @version 1.0
+ */
+
+/**
+ * @description sumaArray suma los valores de un array 
+ * @param {Array} numeros  array de numeros 
+ * @return {number}
+ */
+
+function sumaArray(numeros) {
+    let suma = 0;
+    for (let a of numeros) {
+        suma = suma + a
+    }
+    return suma;
+}
+let sumaValor = sumaArray([2, 3, 4, 5]);
+console.log(sumaValor)
+
+function sumaArray2(numeros) {
+    let suma = 0;
+    for (let pos in numeros) {
+        suma = suma + numeros[pos];
+    }
+    return suma
+}
+
+let sumaValor2 = sumaArray2([2, 3, 4, 5]);
+console.log(sumaValor2)
+
+function sumaArray3(numeros) {
+    let suma = 0;
+    for (let i = 0; i < numeros.length; i++) {
+        suma = suma + numeros[i];
+    }
+    return suma;
+}
+
+let sumaValor3 = sumaArray3([2, 3, 4, 5]);
+console.log(sumaValor3)
